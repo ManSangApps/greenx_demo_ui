66a7e72552c80112314c83eb7020e7b9a0f6ca93
unknown <mansangapps@gmail.com>
2025-12-27 12:39:13 +0530

    #vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   preview: {
-    allowedHosts: "greenx-demo-ui.onrender.com",
+    allowedHosts: ["greenx-demo-ui.onrender.com"],
   },
   plugins: [react()],
 });
